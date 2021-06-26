2a529faa6ee9db3d36afadda05c2395346efe42b
Jaine Senna <jainesantossenna191@gmail.com>
2021-06-26 15:30:31 -0300

    Adding Questao 3

--- T01 - Revisão de Álgebra Relacional, DDL e DML/consultas.sql
+++ T01 - Revisão de Álgebra Relacional, DDL e DML/consultas.sql
@@ -20,3 +20,17 @@ WHERE E.cod_empregado IN (
 	FROM COMPANHIA C
 	WHERE C.nome_companhia LIKE 'Soft Sell')
 );
+
+-- 3) Encontre os nomes, endereço e cidade da residência de todos 
+-- os empregados da Soft Sell que ganham mais de dez mil reais.
+SELECT E.nome_empregado, E.rua, E.cidade
+FROM EMPREGADO E
+WHERE E.cod_empregado IN (	
+	SELECT T.cod_empregado
+	FROM TRABALHA T
+	WHERE T.cod_companhia IN (
+		SELECT C.cod_companhia 
+		FROM COMPANHIA C
+		WHERE C.nome_companhia LIKE 'Soft Sell')
+		AND E.salario > '10.000'
+);
