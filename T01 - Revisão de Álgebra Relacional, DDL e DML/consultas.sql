07a8df9e233553b9eab32019e4c7b215c0fa8a14
Jaine Senna <jainesantossenna191@gmail.com>
2021-06-26 15:25:36 -0300

    Adding Questao 1

---
+++ T01 - Revisão de Álgebra Relacional, DDL e DML/consultas.sql
@@ -0,0 +1,11 @@
+-- 1) Encontre os nomes de todos os empregados que trabalham 
+-- para a Companhia Soft Sell.
+SELECT E.nome_empregado
+FROM EMPREGADO E
+WHERE E.cod_empregado IN (	
+	SELECT T.cod_empregado
+	FROM TRABALHA T
+	WHERE T.cod_companhia IN (SELECT C.cod_companhia
+	FROM COMPANHIA C
+	WHERE C.nome_companhia LIKE  'Soft Sell')
+);
