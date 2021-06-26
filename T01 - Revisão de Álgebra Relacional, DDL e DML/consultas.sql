3e329cc574a3612206d2f6dd8243c953a12908e3
Jaine Senna <jainesantossenna191@gmail.com>
2021-06-26 15:27:11 -0300

    Adding Questao 2

--- T01 - Revisão de Álgebra Relacional, DDL e DML/consultas.sql
+++ T01 - Revisão de Álgebra Relacional, DDL e DML/consultas.sql
@@ -9,3 +9,14 @@ WHERE E.cod_empregado IN (
 	FROM COMPANHIA C
 	WHERE C.nome_companhia LIKE  'Soft Sell')
 );
+
+-- 2) Encontre todos os nomes das cidades dos empregados que trabalham na Soft Sell.
+SELECT E.cidade
+FROM EMPREGADO E
+WHERE E.cod_empregado IN (	
+	SELECT T.cod_empregado
+	FROM TRABALHA T
+	WHERE T.cod_companhia IN (SELECT C.cod_companhia
+	FROM COMPANHIA C
+	WHERE C.nome_companhia LIKE 'Soft Sell')
+);
