57c6530b2e81431317be2da6ae97b16bfd79462c
Jaine Senna <jainesantossenna191@gmail.com>
2021-06-26 15:38:34 -0300

    Adding Questao 6

--- T01 - Revisão de Álgebra Relacional, DDL e DML/consultas.sql
+++ T01 - Revisão de Álgebra Relacional, DDL e DML/consultas.sql
@@ -46,3 +46,15 @@ WHERE E.cod_empregado IN (
 	FROM COMPANHIA C
 	WHERE E.cidade = C.cidade_companhia)
 );
+
+-- 6) Encontre os nomes de todos os empregados, no banco de dados, 
+-- que não trabalham para a Soft Sell.
+SELECT E.nome_empregado
+FROM EMPREGADO E
+WHERE E.cod_empregado IN (	
+	SELECT T.cod_empregado 
+	FROM TRABALHA T
+	WHERE T.cod_companhia IN (SELECT C.cod_companhia
+	FROM COMPANHIA C
+	WHERE C.nome_companhia NOT IN ('Soft Sell'))
+);
